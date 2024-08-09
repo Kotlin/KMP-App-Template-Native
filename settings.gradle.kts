5be838f4a807590e6aa8496c8b51f8d2877af61f
Márton Braun <marton.braun@jetbrains.com>
2024-08-09 10:45:39 +0200

    Update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
         mavenCentral()
 
         // Amper repositories
-        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
         maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
@@ -24,7 +24,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.3.1")
+    id("org.jetbrains.amper.settings.plugin").version("0.4.0")
 }
 
 include(":shared")
+include(":androidApp")
