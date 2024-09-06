6bdc69499edee6f7c34b822ec4609ed710448554
Márton Braun <marton.braun@jetbrains.com>
2024-09-06 10:45:14 +0200

    Update catalog names

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.composeMultiplatform)
 }
 
 kotlin {
