a196a1002e94533c062a05f36d7115e008784400
Márton Braun <marton.braun@jetbrains.com>
2024-09-06 10:51:51 +0200

    Clean up build config

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,10 +41,6 @@ android {
     namespace = "com.jetbrains.kmpapp"
     compileSdk = 34
 
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
     defaultConfig {
         applicationId = "com.jetbrains.kmpapp"
         minSdk = 24
@@ -66,10 +62,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        compose = true
-    }
-    dependencies {
-        debugImplementation(libs.androidx.compose.ui.tooling)
-    }
+}
+
+dependencies {
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
