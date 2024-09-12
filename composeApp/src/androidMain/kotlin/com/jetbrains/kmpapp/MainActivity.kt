e3f334bdbdf6808dd72640e2451458a504edece9
Márton Braun <marton.braun@jetbrains.com>
2024-09-12 09:30:50 +0200

    Reapply edge-to-edge after theme changes

--- composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
@@ -3,13 +3,14 @@ package com.jetbrains.kmpapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,19 +24,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme(
-                colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
-            ) {
-                Surface {
-                    App()
-                }
+            // Remove when https://issuetracker.google.com/issues/364713509 is fixed
+            LaunchedEffect(isSystemInDarkTheme()) {
+                enableEdgeToEdge()
             }
+            App()
         }
     }
 }
 
 @Composable
 fun App() {
+    MaterialTheme(
+        colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
+    ) {
+        Surface {
+            NavigationContent()
+        }
+    }
+}
+
+@Composable
+fun NavigationContent() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "list") {
         composable("list") {
