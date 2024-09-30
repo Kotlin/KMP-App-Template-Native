acce4b0a2952388c42f22110074bf80f07aa6dee
Márton Braun <marton.braun@jetbrains.com>
2024-09-30 14:19:24 +0200

    Fix imports

--- composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
@@ -5,20 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
-import com.jetbrains.kmpapp.screens.DetailDestination
-import com.jetbrains.kmpapp.screens.DetailScreen
-import com.jetbrains.kmpapp.screens.ListDestination
-import com.jetbrains.kmpapp.screens.ListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
