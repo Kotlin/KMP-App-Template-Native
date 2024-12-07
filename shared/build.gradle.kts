9a479440af92c7f5f6a9dd73aaa6e8c07a880062
horatiothomas <horatiothomas@gmail.com>
2024-12-07 16:05:32 +0800

    Add iOS previews

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "Shared"
-            isStatic = true
+            isStatic = false
         }
     }
 
